c4b086cf1a2c051452ba87da1c077a85def63578
Rohan Raut <Rohanraut2300@gmail.com>
2024-02-16 10:30:22 +0530

    added bot link

--- DATABASE FILE/employeeleavedb.sql
+++ DATABASE FILE/employeeleavedb.sql
@@ -11,10 +11,10 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+--/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+--/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+--/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+--/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `employeeleavedb`
@@ -112,7 +112,6 @@ INSERT INTO `tblemployees` (`id`, `EmpId`, `FirstName`, `LastName`, `EmailId`, `
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `tblleaves`
 --
 
--- index.php
+++ index.php
@@ -111,6 +111,10 @@
             </div>
         </div>
     </div>
+    <center>
+        <iframe src='https://webchat.botframework.com/embed/RRLanguageService-bot?s=MPVyZiDKeEE.zRcqrcZKhLDJ4afxwmpEavaCI3w2X9DtBmpbvZv2kZE'  style='min-width: 300px; width: 60%; min-height: 250px;'></iframe>
+        <h2 style="font-family: Arial, sans-serif; text-align: center; margin-top: 100px;color: #333;">Query Bot</h2>
+    </center>
     <!-- login area end -->
 
     <!-- jquery latest version -->
